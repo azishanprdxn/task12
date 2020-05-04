eeac4ec237c722d1a3820a27ffd9651b74c39817
Zishan Ansari <zshan.ansari07@gmail.com>
2020-05-04 12:48:56 +0530

    Added Slick Slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 // JavaScript here
+
+$(document).ready(function () {
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+});
